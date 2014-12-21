5b0b73f4b4052edbb1a7caf8b5b32864a6e8efb4
ILYA ERSHOV <ershov.ilya@gmail.com>
2014-12-21 11:14:36 +0300

    Подключил языковые переменные на Angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,19 @@
 /**
  * Created by Ilya on 21.12.2014.
  */
+(function(){
+
+console.log('Hello from angular.js');
+var app = angular.module('core', [ ]);
+
+app.controller('LangController', function() {
+  this.word=RU;
+});
+
+var RU={
+  'page':'страница',
+  'Page':'Страница',
+  'footer':'Футер'
+};
+
+})();

62849f73aa1a0a7ea2db9495de05bfd8ee8fdbc1
ILYA ERSHOV <ershov.ilya@gmail.com>
2014-12-21 13:15:56 +0300

    Добавлена поддержка многоязычности

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,20 +3,46 @@
  */
 (function(angular){
 
-  console.log('Hello from angular.js');
+  console.log('Hello from app.js');
   var app = angular.module('core', ['ngSanitize']);
 
   app.controller('CoreController', ['$scope', function($scope) {
-    this.word=RU;
-    $scope.footer = footer;
+    var locale=getLocale('RU');
+    this.word=locale.word;
+    $scope.footer = locale.chunk.footer;
   }]);
 
-  var footer = '<a href="#page">&laquo; назад</a>';
+  // Локализации
+  function getLocale(localeID){
+    switch(localeID){
+      case 'RU':
+        var obj={word: RU};
+        break;
+      case 'EN':
+        var obj={word: EN};
+        break;
+      default:
+        var obj={word: EN};
+    }
+    obj.chunk={};
+    obj.chunk.footer='<a href="#page">&laquo; '+obj.word.back+'</a>';
+    return obj;
+  }
 
   var RU={
+    'content':'содержимое',
+    'back':'в начало',
     'page':'страница',
     'Page':'Страница',
     'footer':'Футер'
+  };
+
+  var EN={
+    'content':'content',
+    'back':' go back',
+    'page':'page',
+    'Page':'Page',
+    'footer':'Footer'
   };
 
 })(window.angular);

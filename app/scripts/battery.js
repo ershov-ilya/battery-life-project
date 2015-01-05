fde7052fcfe86a07b354e7567a28354d1f3dbb10
ILYA ERSHOV <ershov.ilya@gmail.com>
2015-01-05 14:32:05 +0300

    Инициализирован контроллер батарейки

---
+++ app/scripts/battery.js
@@ -0,0 +1,19 @@
+/**
+ * Created by
+ * User: ershov-ilya
+ * Website: ershov.pw
+ * GitHub : https://github.com/ershov-ilya
+ * on 05.01.2015.
+ */
+
+var BATTERY = (function(){
+  // Private methods and data
+  var width = 500;
+  var height = 1000;
+
+  // Public methods
+  return {
+    test: function(){ console.log('Works fine!');}
+  };
+
+})();

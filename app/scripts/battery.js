b92bc439b6745cc24dae64cf0cb120af533ae31a
ILYA ERSHOV <ershov.ilya@gmail.com>
2015-01-05 16:12:50 +0300

    Родился метод BATTERY.setColor()

--- app/scripts/battery.js
+++ app/scripts/battery.js
@@ -8,12 +8,53 @@
 
 var BATTERY = (function(){
   // Private methods and data
+  var DEBUG=true;
   var width = 500;
   var height = 1000;
 
+  function buildDelayedFuncSetColor(key) {
+    return function () {
+      console.log(key);
+      $('.fluid').css({fill:'url(#SVGID_fluid_'+key+')'});
+      $('.surface').css({fill:'url(#SVGID_surface_'+key+')'});
+    }
+  }
   // Public methods
   return {
-    test: function(){ console.log('Works fine!');}
+    test: function(){ console.log('Works fine!');},
+
+    setColor: function(value){
+      if(typeof value === "string"){
+        value = parseFloat(value, 0);
+      }
+      //var surface='green', fluid='green';
+      var rainbow={
+        green:95,
+        salad:80,
+        yellow:50,
+        orange:33.3,
+        orangered:25,
+        brown:11,
+        red:0
+      };
+      var wait= 0, color='green';
+
+      for(key in rainbow){
+        // Назначение цвета в цикле
+
+        var rainbowfunc={};
+        // Перебор цветов
+        color=key;
+        if(value>=rainbow[key]) break;
+        //wait+=100;
+      }
+      $('.fluid').css({fill:'url(#SVGID_fluid_'+color+')'});
+      $('.surface').css({fill:'url(#SVGID_surface_'+color+')'});
+
+      if(DEBUG) console.log(color);
+      return true;
+    }
+
   };
 
-})();
+})(); // BATTERY end
